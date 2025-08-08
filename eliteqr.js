1b250035fb578044ca6372617fdd2933afc9ea7d
Mastetech-XD <masterpeaceelite@gmail.com>
2025-08-08 17:42:20 +0300

    Add files via upload

--- eliteqr.js
+++ eliteqr.js
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function MASTERTECH_XD_QR_CODE() {
+	async function WASI_MD_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Elite_Tech = Elite_Tech({
+			let Qr_Code_By_Wasi_Tech = Wasi_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Elite_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Elite_Tech.ev.on("connection.update", async (s) => {
+			Qr_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,9 +56,9 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Elite_Tech.sendMessage(Qr_Code_By_Elite_Tech.user.id, { text: '' + b64data });
+				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
-				   let MASTERTECH_XD_TEXT = `
+				   let WASI_MD_TEXT = `
 *_Session Connected By MASTERTECH_*
 *_Made With 🤍_*
 ______________________________________
@@ -77,16 +77,16 @@ ______________________________________
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Elite_Tech.sendMessage(Qr_Code_By_Elite_Tech.user.id,{text:MASTERTECH_XD_TEXT},{quoted:session})
+	 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Elite_Tech.ws.close();
+					await Qr_Code_By_Wasi_Tech.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					MASTERTECH_XD_QR_CODE();
+					WASI_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
@@ -99,6 +99,6 @@ _Don't Forget To Give Star To My Repo_`
 			await removeFile("temp/" + id);
 		}
 	}
-	return await MASTERTECH_XD_QR_CODE()
+	return await WASI_MD_QR_CODE()
 });
 module.exports = router
